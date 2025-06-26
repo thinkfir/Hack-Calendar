2e7eddf16b0614aba5b67c613148b0dac620b011
Thinkfir <aryaaithal@gmail.com>
2025-06-26 01:17:19 -0400

    frontend making

---
+++ hack-calendar-fullstack/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskGenerator from './components/TaskGenerator';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hack Calendar</h1>
+      <TaskGenerator />
+    </div>
+  );
+};
+
+export default App;
